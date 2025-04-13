1655bd1409e2b2a83bc112625cb07595bdf237ec
cruz-jay <jay-cruz@outlook.com>
2025-04-12 20:14:42 -0700

    attempt 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Navigate } from "react-router-dom";
 
-import Home from "./home/Home";
-import MainPage from "./useX86/src/pages/MainPage";
-import DataPage from "./useX86/src/pages/DataPage";
+// Use absolute imports instead of relative ones
+import Home from "/src/home/Home";
+import MainPage from "/src/useX86/src/pages/MainPage";
+import DataPage from "/src/useX86/src/pages/DataPage";
 
-import WorldLayout from "./usemap2/src/pages/WorldLayout";
-import PastCities from "./usemap2/src/pages/PastCities";
-import FutureCityList from "./usemap2/src/pages/FutureCityList";
-import { NewCitiesProvider } from "./usemap2/src/Context/Data";
-import { FutureCityProvider } from "./usemap2/src/Context/Data_two";
+import WorldLayout from "/src/usemap2/src/pages/WorldLayout";
+import PastCities from "/src/usemap2/src/pages/PastCities";
+import FutureCityList from "/src/usemap2/src/pages/FutureCityList";
+import { NewCitiesProvider } from "/src/usemap2/src/Context/Data";
+import { FutureCityProvider } from "/src/usemap2/src/Context/Data_two";
 
-import ProjectTwoHome from "./usemap2/src/pages/ProjectTwoHome";
-import ProjectThreeHome from "./useX86/src/pages/ProjectThreeHome";
+import ProjectTwoHome from "/src/usemap2/src/pages/ProjectTwoHome";
+import ProjectThreeHome from "/src/useX86/src/pages/ProjectThreeHome";
 
-import UseMap_Home from "./home/useMap_Home";
-import UseX86_Home from "./home/useX86_Home";
-import UseBeatBot_Home from "./home/UseBeatBot_Home";
+import UseMap_Home from "/src/home/UseMap_Home";
+import UseX86_Home from "/src/home/UseX86_Home";
+import UseBeatBot_Home from "/src/home/UseBeatBot_Home";
 
 const router = createBrowserRouter([
   {
