67bb2ed7e325c01a885464ba0f063ebbfc313dad
cruz-jay <jay-cruz@outlook.com>
2025-04-12 20:01:54 -0700

    attempt 2

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
 import autoprefixer from "autoprefixer";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
@@ -15,8 +16,17 @@ export default defineConfig({
       ],
     },
   },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      src: path.resolve(__dirname, "./src"),
+    },
+  },
+
   assetsInclude: ["**/*.otf", "**/*.ttf", "**/*.woff", "**/*.woff2"],
   build: {
     assetsInlineLimit: 0,
+    outDir: "dist",
+    sourcemap: true,
   },
 });
